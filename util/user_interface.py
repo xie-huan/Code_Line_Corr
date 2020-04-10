6cad42646159721dc2ffa479c2d519694ed8cf1e
xx790008409 <790008409@qq.com>
2020-04-10 13:48:41 +0800

    little changes

--- util/user_interface.py
+++ util/user_interface.py
@@ -18,7 +18,7 @@ def start_calc_corr(start_dir):
             start_calc_corr(curr_path)
         elif os.path.isfile(curr_path):
             # 如果不包含covMatrix.txt、error.txt、componentinfo.txt三个文件，则不进行读取数据操作
-            correct_file_list = ['covMatrix.txt','error.txt','componentinfo.txt']
+            correct_file_list = ['covMatrix.txt', 'error.txt', 'componentinfo.txt']
             if not set(correct_file_list).issubset(set(curr_file_list)):
                 continue
 
@@ -27,8 +27,8 @@ def start_calc_corr(start_dir):
 
             # 计算相关系数并输出到txt文件中
             print("计算" + os.path.dirname(curr_path) + "的相关系数:")
-            # method_list = ["pearson", "spearman", "kendall"]
-            method_list = ["mutual information", "chisquare"]
+            method_list = ["pearson", "spearman", "kendall"]
+            # method_list = ["mutual information", "chisquare"]
 
             for concrete_method in method_list:
                 concrete_corr = calc_corr_bymyself(data, concrete_method)
@@ -56,12 +56,17 @@ def start_calc_rank(start_dir):
                 continue
             curr_file_list = [curr_file_list.remove(file) for file in correct_file_list]
 
-            # df            df              df              list
-            pearson_corr, spearman_corr, kendall_corr, fault_line_data = get_corr(os.path.dirname(curr_path))
-            rank_dict = calc_rank(pearson_corr, spearman_corr, kendall_corr, fault_line_data)
+            all_df_dict, fault_line_data = get_corr(os.path.dirname(curr_path))
+            rank_dict = calc_rank(all_df_dict, fault_line_data)
             write_rank_to_txt(curr_path, rank_dict)
 
 
+# 读取排名，计算百分比，存入rank_percent.txt文件中
+def start_rank_percent(dir):
+    pass
+
+
+# 绘图
 def start_plot(start_dir):
     pre_dir = start_dir
 
