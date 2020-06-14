9e27c2172a22d4b396e51a782c0729cd221afe05
xx790008409 <790008409@qq.com>
2020-06-14 09:15:55 +0800

    fix bugs about dstar, ochiai, barinel

--- corr_core/metrics.py
+++ corr_core/metrics.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas as pd
 from scipy import stats
 
+
 # Pearson 相关系数的实现，给定特征和标签series，返回相关系数值
 def pearson(feature, label):
+    # 自己实现的pearson算法
     cov = feature.cov(label)  # 协方差
     std_x = feature.std()  # 标准差
     std_y = label.std()  # 标准差
@@ -11,8 +13,14 @@ def pearson(feature, label):
         return np.nan
     else:
         pearson_corr = cov / (std_x * std_y)  # 特征与标签的相关系数
-        pearson_corr = min(1., max(-1., pearson_corr))  # 限制结果范围区间为[-1, 1]
+        # pearson_corr = min(1., max(-1., pearson_corr))  # 限制结果范围区间为[-1, 1]
         return round(pearson_corr, 6)  # 浮点数精度不准
+
+    # 调用stats实现的pearson
+    # try:
+    #     return stats.pearsonr(feature, label)[0]
+    # except:
+    #     return np.nan
 
 
 # Spearman 相关系数的实现，给定特征和标签series，返回相关系数值
@@ -39,8 +47,14 @@ def spearman(feature, label):
         return np.nan
     else:
         spearman_corr = cov / (std_x * std_y)  # 特征与标签的相关系数
-        spearman_corr = min(1., max(-1., spearman_corr))  # 限制结果范围区间为[-1, 1]
+        # spearman_corr = min(1., max(-1., spearman_corr))  # 限制结果范围区间为[-1, 1]
         return round(spearman_corr, 6)
+
+    # 调用stats实现的spearman
+    # try:
+    #     return stats.spearmanr(feature, label)[0]
+    # except:
+    #     return np.nan
 
 
 # 计算kendall相关系数接口
@@ -58,7 +72,7 @@ def kendall(feature, label):
     x, y = x[perm], y[perm]
     x = np.r_[True, x[1:] != x[:-1]].cumsum(dtype=np.intp)
 
-    dis = calc_dis(y)    # discordant pairs
+    dis = calc_dis(y)  # discordant pairs
 
     obs = np.r_[True, (x[1:] != x[:-1]) | (y[1:] != y[:-1]), True]
     cnt = np.diff(np.nonzero(obs)[0]).astype('int64', copy=False)
@@ -93,10 +107,10 @@ def mergeSortInversion(data, aux, low, high):
     mid = low + (high - low) // 2
     # 递归调用过程
     leftCount = mergeSortInversion(data, aux, low, mid)
-    rightCount = mergeSortInversion(data, aux, mid+1, high)
+    rightCount = mergeSortInversion(data, aux, mid + 1, high)
 
     # merge 过程
-    for index in range(low, high+1):
+    for index in range(low, high + 1):
         aux[index] = data[index]
     count = 0
     i = low
@@ -124,7 +138,7 @@ def mergeSortInversion(data, aux, low, high):
 # 计算kendall相关系数中的不一致对
 def calc_dis(y):
     aux = [y[i] for i in range(len(y))]
-    nSwap = mergeSortInversion(y, aux, 0, len(y)-1)
+    nSwap = mergeSortInversion(y, aux, 0, len(y) - 1)
     return nSwap
 
 
@@ -132,33 +146,73 @@ def calc_dis(y):
 # c表示cover，u表示uncover
 # f表示failure，s表示success
 def get_N_para(feature, label):
-    N = len(label)
-    success = feature[label==0]
-    failure = feature[label==1]
+    # N = len(label)
+    # success = feature[label==0]
+    # failure = feature[label==1]
+    #
+    # Ncf = np.sum(failure)
+    # Nuf = N - Ncf
+    #
+    # Ncs = np.sum(success)
+    # Nus = N - Ncs
+    feature = np.array(feature)
+    label = np.array(label)
+    Ncf = np.sum(feature[label == 1])
+    Nuf = np.sum(label == 1) - Ncf
 
-    Ncf = np.sum(failure)
-    Nuf = N - Ncf
-
-    Ncs = np.sum(success)
-    Nus = N - Ncs
+    Ncs = np.sum(feature[label == 0])
+    Nus = np.sum(label == 0) - Ncs
 
     return Ncf, Nuf, Ncs, Nus
+
+
+# @xiehuan实现的卡方检验
+def chisq(feature, label):
+    # 实际值
+    obs = list(get_N_para(feature, label))
+
+    # 计算理论值
+    fail_ratio = np.sum(label) / len(label)
+    success_ratio = 1 - fail_ratio
+
+    cover = np.sum(feature)
+    uncover = len(feature) - cover
+
+    Ncf = cover * fail_ratio
+    Nuf = uncover * fail_ratio
+    Ncs = cover * success_ratio
+    Nus = uncover * success_ratio
+    exp = [Ncf, Nuf, Ncs, Nus]
+
+    return 1 - stats.chisquare(obs, exp)[1]
 
 
 # Dstar
 def dstar(feature, label):
     Ncf, Nuf, Ncs, Nus = get_N_para(feature, label)
-    return round(Ncf**2/(Ncs+Nuf),6)
+    try:
+        return round(Ncf ** 2 / (Ncs + Nuf), 6)
+    except:
+        return 0.0
+
 
 # Ochiai
 def ochiai(feature, label):
     Ncf, Nuf, Ncs, Nus = get_N_para(feature, label)
-    return round(Ncf/np.sqrt((Ncf+Nuf)*(Ncf+Ncs)),6)
+    try:
+        return round(Ncf / np.sqrt((Ncf + Nuf) * (Ncf + Ncs)), 6)
+    except:
+        return 0.0
 
-#Barinel
+
+# Barinel
 def barinel(feature, label):
     Ncf, Nuf, Ncs, Nus = get_N_para(feature, label)
-    return round(1-Ncs/(Ncs+Ncf),6)
+    try:
+        return round(1 - Ncs / (Ncs + Ncf), 6)
+    except:
+        return 0.0
+
 
 # 时间复杂度较高的kendall相关系数实现，已优化，暂不使用
 def k(data):
@@ -198,15 +252,16 @@ def k(data):
 """
 
 import math
-#标签只能为0和1,样本空间任意
-#samle:n*m 的列表
-#label:m*1 的列表
-#返回每个特征的fisher score值的一个列表
-#eg:  sample = [[1,2,3],[1,0,1],[1,5,6]]
-#     label = [1, 0, 1]
-#return  lst=[nan, 1.8148148148148149, 1.8148148148148149]
-def binary_fisher_score(sample,label):
 
+
+# 标签只能为0和1,样本空间任意
+# samle:n*m 的列表
+# label:m*1 的列表
+# 返回每个特征的fisher score值的一个列表
+# eg:  sample = [[1,2,3],[1,0,1],[1,5,6]]
+#     label = [1, 0, 1]
+# return  lst=[nan, 1.8148148148148149, 1.8148148148148149]
+def binary_fisher_score(sample, label):
     if len(sample) != len(label):
         print('Sample does not match label')
         exit()
@@ -214,31 +269,31 @@ def binary_fisher_score(sample,label):
     df2 = pd.DataFrame(label, columns=['label'])
     data = pd.concat([df1, df2], axis=1)  # 合并成为一个dataframe
 
-    data0 = data[data.label == 0]#对标签分类，分成包含0和1的两个dataframe
+    data0 = data[data.label == 0]  # 对标签分类，分成包含0和1的两个dataframe
     data1 = data[data.label == 1]
-    n = len(label)#标签长度
-    n1 = sum(label)#1类标签的个数
-    n0 = n - n1#0类标签的个数
-    lst = []#用于返回的列表
+    n = len(label)  # 标签长度
+    n1 = sum(label)  # 1类标签的个数
+    n0 = n - n1  # 0类标签的个数
+    lst = []  # 用于返回的列表
     features_list = list(data.columns)[:-1]
     for feature in features_list:
 
         # 算关于data0
         m0_feature_mean = data0[feature].mean()  # 0类标签在第m维上的均值
         # 0类在第m维上的sw
-        m0_SW=sum((data0[feature] -m0_feature_mean )**2)
+        m0_SW = sum((data0[feature] - m0_feature_mean) ** 2)
         # 算关于data1
         m1_feature_mean = data1[feature].mean()  # 1类标签在第m维上的均值
         # 1类在第m维上的sw
-        m1_SW=sum((data1[feature] -m1_feature_mean )**2)
+        m1_SW = sum((data1[feature] - m1_feature_mean) ** 2)
         # 算关于data
         m_all_feature_mean = data[feature].mean()  # 所有类标签在第m维上的均值
 
         m0_SB = n0 / n * (m0_feature_mean - m_all_feature_mean) ** 2
         m1_SB = n1 / n * (m1_feature_mean - m_all_feature_mean) ** 2
-        #计算SB
+        # 计算SB
         m_SB = m1_SB + m0_SB
-        #计算SW
+        # 计算SW
         m_SW = (m0_SW + m1_SW) / n
         if m_SW == 0:
             # 0/0类型也是返回nan
@@ -246,16 +301,15 @@ def binary_fisher_score(sample,label):
         else:
             # 计算Fisher score
             m_fisher_score = m_SB / m_SW
-        #Fisher score值添加进列表
-        lst.append(round(m_fisher_score,6))
+        # Fisher score值添加进列表
+        lst.append(round(m_fisher_score, 6))
 
     return lst
 
 
-
 # 针对标签样本都是二值(0和1)的互信息,label和sample是对称的
-#eg:label=[0,1,0]   sample=[1,0,1]
-#return 0.6365141682948128
+# eg:label=[0,1,0]   sample=[1,0,1]
+# return 0.6365141682948128
 def binary_mutula_information(label, sample):
     # 用字典来计数
     d = dict()
@@ -329,7 +383,8 @@ def log_igf(s, z):
     k = float(KM(s, z))
     return math.log(k) + sc
 
-#卡方检验求p值
+
+# 卡方检验求p值
 # dof是自由度，chi_squared为卡方值，该函数实现知道自由度和卡方值求p值
 # 核心是用不完全伽马函数除以伽马函数，两者都采用近似函数求解
 # 参见https://blog.csdn.net/idatamining/article/details/8565042
@@ -346,7 +401,7 @@ def chisqr2pValue(dof, chi_squared):
     # 不完全伽马函数，采用计算其log值
     incompleteGamma = log_igf(k, v)
     # 如果过小或者无穷
-    if math.exp(incompleteGamma) <= 1e-8 or math.exp(incompleteGamma) == float('inf'):
+    if incompleteGamma == float('-inf') or incompleteGamma == float('inf'):
         return 1e-14
 
     # 完全伽马函数，用斯特林公式近似
@@ -359,8 +414,8 @@ def chisqr2pValue(dof, chi_squared):
 
 
 # 自己实现chisquare,参数为两个列表obs,exp，返回为包含卡方值和p值的列表
-#eg:obs=[8,7,7] ,exp=[8,8,8]
-#return [0.25, 0.8824969025845955]
+# eg:obs=[8,7,7] ,exp=[8,8,8]
+# return [0.25, 0.8824969025845955]
 def my_chisquare(obs, exp):
     # 将列表转化为numpy.ndarray类型
     obs = np.atleast_1d(np.asanyarray(obs))
@@ -376,7 +431,7 @@ def my_chisquare(obs, exp):
     # 计算obs,exp的维度
     num_obs = terms.size
     # 调用自己写的求p值的函数，得到p值
-    p = round(chisqr2pValue(num_obs - 1, stat),6)
+    p = round(chisqr2pValue(num_obs - 1, stat), 6)
     chisquare_list = []
     chisquare_list.append(stat)
     chisquare_list.append(p)
